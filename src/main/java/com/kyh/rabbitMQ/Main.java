3c295ad5ac9cfea35474baafaa90449b6a954263
孔昀晖 <1032316751@qq.com>
2017-03-30 15:20:28 +0800

    topic 主题交换机 例如生产者发送baidu.dept1, 消费者1(bindingKey=“baidu.*”)和消费者2(bindingKey=“*.dept1”)都可以接收到该消息 和direct类似，引入了模糊匹配（主题）

--- src/main/java/com/kyh/rabbitMQ/Main.java
+++ src/main/java/com/kyh/rabbitMQ/Main.java
@@ -12,23 +12,40 @@ public class Main {
         // 消费者线程，接收消息
         ExecutorService executors = Executors.newFixedThreadPool(3);
         for(int i=0; i<3; i++){
-            QueueConsumer consumer = new QueueConsumer("direct_logs", "level" + (i+1));
+            // *（星）替代一个单词。
+            // ＃（哈希）替换零个或多个单词。
+            String bindingKey="";
+            switch (i){
+                case 0:
+                    bindingKey = "kern.*";
+                    break;
+                case 1:
+                    bindingKey = "*.critical";
+                    break;
+                case 2:
+                    bindingKey = "#";
+                    break;
+            }
+            QueueConsumer consumer = new QueueConsumer("topic_logs", bindingKey);
             executors.submit(consumer);
         }
 
         // 生产者，发送消息
-        Producer producer = new Producer("direct_logs");
-        for (int i = 0; i < 10; i++) {
-            if(i<3) producer.setBindingKey("level1");
-            else if(i<6) producer.setBindingKey("level2");
-            else producer.setBindingKey("level3");
-
+        Producer producer = new Producer("topic_logs");
+        for (int i = 0; i < 2; i++) {
+            switch (i){
+                case 0:
+                    producer.setBindingKey("kern.critical"); // 这条消息，被C1、C2、C3接收了
+                    break;
+                case 1:
+                    producer.setBindingKey("A critical kernel error"); // 这条消息，被C3接收
+                    break;
+            }
             HashMap message = new HashMap();
             message.put("message number", i);
             producer.sendMessage(message);
             System.out.println("Message Number " + i + " sent.");
         }
-        producer.close();
     }
 
     /**
