5fb5d9477668cab9e9e802433cacfc5692eb1f94
孔昀晖 <1032316751@qq.com>
2017-05-10 10:32:15 +0800

    aop

---
+++ src/main/java/com/kyh/aop/WebLogAspect.java
@@ -0,0 +1,51 @@
+package com.kyh.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+//@Order(5) // 优先级, 1-10，值越小优先级越高 (doBefore - doAfterReturning: o5:o10 - o10:o5 )
+@Component
+public class WebLogAspect {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    ThreadLocal<Long> startTime = new ThreadLocal<>(); // 使用ThreadLocal,解决aop中的同步问题
+
+    @Pointcut("execution(public * com.kyh.controller..*.*(..))")
+    public void webLog() {}
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        // 计时开始
+        startTime.set(System.currentTimeMillis());
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        // 记录下请求内容
+        logger.info("URL : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        logger.info("RESPONSE : " + ret);
+        // 打印请求时长
+        logger.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+    }
+}
