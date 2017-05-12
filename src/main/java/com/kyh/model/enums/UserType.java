f2d48e4f4a447b5be09acfea90483aa5f7ad7dfc
孔昀晖 <1032316751@qq.com>
2017-05-12 16:26:31 +0800

    枚举类型注释修改

--- src/main/java/com/kyh/model/enums/UserType.java
+++ src/main/java/com/kyh/model/enums/UserType.java
@@ -5,7 +5,7 @@ package com.kyh.model.enums;
  *
  * 关于枚举类型映射：
  * 1、对应的数据库字段类型是"字符串类型"，则直接用javaType处理就行，因为默认使用了EnumTypeHandler
- * 2、对应的数据库字段类型是"字符串类型"，则需要使用typeHandler=org.apache.ibatis.type.EnumOrdinalTypeHandler
+ * 2、对应的数据库字段类型是int/char(1)，则需要使用typeHandler=org.apache.ibatis.type.EnumOrdinalTypeHandler
  *
  * 备注：如果是复杂类型映射：需要创建BaseTypeHandler<T>子类，typeHandler=该子类
  *                        或者该子类加上@MappedTypes(clz)注解，properties中指定路径
