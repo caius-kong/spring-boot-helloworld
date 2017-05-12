f5280b6f290d2d256082c8a8c9a091fdf91b53b1
孔昀晖 <1032316751@qq.com>
2017-05-12 15:55:48 +0800

    mybatis枚举类型映射备注

--- src/main/java/com/kyh/model/enums/UserType.java
+++ src/main/java/com/kyh/model/enums/UserType.java
@@ -2,6 +2,13 @@ package com.kyh.model.enums;
 
 /**
  * Created by kongyunhui on 2017/5/12.
+ *
+ * 关于枚举类型映射：
+ * 1、对应的数据库字段类型是"字符串类型"，则直接用javaType处理就行，因为默认使用了EnumTypeHandler
+ * 2、对应的数据库字段类型是"字符串类型"，则需要使用typeHandler=org.apache.ibatis.type.EnumOrdinalTypeHandler
+ *
+ * 备注：如果是复杂类型映射：需要创建BaseTypeHandler<T>子类，typeHandler=该子类
+ *                        或者该子类加上@MappedTypes(clz)注解，properties中指定路径
  */
 public enum UserType {
     USER("USER", "普通用户"), ADMIN("ADMIN", "系统管理员");
