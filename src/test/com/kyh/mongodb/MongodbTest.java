5cd16b80628a0a2855561e559f38c527ec26c9c9
孔昀晖 <1032316751@qq.com>
2017-06-12 16:25:39 +0800

    remove mongodb op

--- src/test/com/kyh/mongodb/MongodbTest.java
+++ src/test/com/kyh/mongodb/MongodbTest.java
@@ -6,10 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.MongoDbUtils;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
@@ -18,22 +14,22 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
 public class MongodbTest {
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Test
-    public void test1(){
-        User user = new User();
-        user.setId(1l);
-        user.setUsername("kong");
-        user.setPassword("123");
-        mongoTemplate.save(user);
-    }
-
-    @Test
-    public void test2(){
-        Query query = new Query(Criteria.where("username").is("kong"));
-        User one = mongoTemplate.findOne(query, User.class);
-        System.out.println(one);
-    }
+//    @Autowired
+//    private MongoTemplate mongoTemplate;
+//
+//    @Test
+//    public void test1(){
+//        User user = new User();
+//        user.setId(1l);
+//        user.setUsername("kong");
+//        user.setPassword("123");
+//        mongoTemplate.save(user);
+//    }
+//
+//    @Test
+//    public void test2(){
+//        Query query = new Query(Criteria.where("username").is("kong"));
+//        User one = mongoTemplate.findOne(query, User.class);
+//        System.out.println(one);
+//    }
 }
